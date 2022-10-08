9360c670fad54150cecbbca7a65e2cfc4e88e7f8
Eduardo Cardoso <eduardocardoso1039@gmail.com>
2022-10-08 02:40:44 -0300

    excluida variavel da api no node

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const APInode = "d63bfabd0c861ed3e5d836ba9c75e4ba"
 
 const app = express();
 app.set("view engine", "ejs");
